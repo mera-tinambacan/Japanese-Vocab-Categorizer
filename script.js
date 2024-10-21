375f7183d96a307d2c238a6406fcb7d4dd97173b
mera-tinambacan <meracle.tinambacan@awsys-i.com>
2024-10-21 16:26:16 +0800

    commiting before pulling

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.getElementById('processButton').addEventListener('click', async () => {
     console.log("Input text:", textInput);
 
     try {
-        const response = await fetch('https://japanese-vocab-categorizer.vercel.app/api/server.js');
+        const response = await fetch('http://localhost:5000/api/server');
+        // const response = await fetch('/api/server');
         if (!response.ok) {
             throw new Error("Failed to fetch vocab");
         }
