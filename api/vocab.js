0318814e2a3a6d6fddbe9c926f730dc58b464107
mera-tinambacan <meracle.tinambacan@awsys-i.com>
2024-10-27 11:23:33 +0800

    moved mongo_uri to .env

--- api/vocab.js
+++ api/vocab.js
@@ -5,14 +5,19 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+require('dotenv').config({ path: '../.env' });
+
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect("mongodb+srv://admin:admin@zuittbatch243.azu7fek.mongodb.net/Nihongo?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB successfully");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
 
 const vocabSchema = new mongoose.Schema({
     word: String,
